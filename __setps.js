90911cf5f20dafd37311050ec2c6125562c5b586
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-04-21 00:25:31 +0600

    added setps file

---
+++ __setps.js
@@ -0,0 +1,15 @@
+/**
+ * 1. create afolder (manually ot mkdir)
+ * 2. open command line to that folder
+ * 3. npm init
+ * 4. npm install express
+ * 5. create index.js
+ * ----------------------------------------------
+ *   For Express/node server
+ * ----------------------------------------------
+ * 1. require app variable
+ * 2. create app variable
+ * 3. declare port
+ * 4. set app.get('/)
+ * 
+ * **/
